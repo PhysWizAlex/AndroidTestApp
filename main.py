384c6fe2edaba4d1c274e7c294f1e328bfa32812
PhysWizAlex <88382738+PhysWizAlex@users.noreply.github.com>
2021-08-03 21:18:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import kivy
+from kivymd.app import MDApp
+from kivymd.uix.list import TwoLineListItem, MDList
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivymd.uix.picker import MDDatePicker
+
+Builder.load_file('whatever.kv')
+
+class MyLayout(Widget):
+    def select_start_date(self,instance,value,date_range):
+        self.ids.start_date_label.text = str(value)
+        
+    def select_end_date(self,instance,value,date_range):
+        self.ids.end_date_label.text = str(value)
+        
+    def selected_object(self, objId):
+        print('click', objId)
+        response = requests.get("https://api.nasa.gov/neo/rest/v1/neo/" + str(objId) + "?api_key=eLLQHjpU93iCInMRF8Cvrm3x2wLoQrtDGsinjF48")
+        responseDump = json.dumps(response.json(),sort_keys=True, indent=4)
+        estDiameter = json.loads(responseDump)['estimated_diameter']['meters']
+        # CAD = json.loads(responseDump)['close_approach_data']
+        loadedDump = json.loads(responseDump)['orbital_data']
+        
+        self.ids.ap.text = 'Aphelion (Furtherst) Distance (AU): ' + str(round(float(loadedDump['aphelion_distance']),2))
+        self.ids.per.text = 'Perihelion (Closest) Distance (AU): ' + str(round(float(loadedDump['perihelion_distance']),2))
+        self.ids.per_arg.text = 'Perihelion Argument (deg): ' + str(round(float(loadedDump['perihelion_argument']),2))
+        self.ids.orb_period.text = 'Orbital Period (Days): ' + str(round(float(loadedDump['orbital_period']),2))
+        self.ids.mean_motion.text = 'Mean Motion (deg/d): ' + str(round(float(loadedDump['mean_motion']),2))
+        self.ids.sem_maj_ax.text = 'Semi Maj Ax. (AU): ' + str(round(float(loadedDump['semi_major_axis']),2))
+        self.ids.asc_node_long.text = 'Ascending Node Longitude (deg): ' + str(round(float(loadedDump['ascending_node_longitude']),2))
+        self.ids.incl.text = 'Inclination (deg): ' + str(round(float(loadedDump['inclination']),2))
+        self.ids.est_dia.text = 'Est. Diameter (meters)(max/min): ' + str(round(estDiameter['estimated_diameter_max'],2)) + '/' + str(round(estDiameter['estimated_diameter_min'],2))
+        
+        
+        # periDistLab.config(text = 'Perihelion (Closest) Distance (AU): ' + str(round(float(loadedDump['perihelion_distance']),2)))
+        # periArgLab.config(text = 'Perihelion Argument (deg): ' + str(round(float(loadedDump['perihelion_argument']),2)))
+        # periodLab.config(text = 'Orbital Period (Days): ' + str(round(float(loadedDump['orbital_period']),2)))
+        # meanMotionLab.config(text = 'Mean Motion (deg/d): ' + str(round(float(loadedDump['mean_motion']),2)))
+        # semiMajAxisLab.config(text = 'Semi Maj Ax. (AU): ' + str(round(float(loadedDump['semi_major_axis']),2)))
+        # ascNodeLongLab.config(text = 'Ascending Node Longitude (deg): ' + str(round(float(loadedDump['ascending_node_longitude']),2)))
+        # incLab.config(text = 'Inclination (deg): ' + str(round(float(loadedDump['inclination']),2)))
+        # estDiaLab.config(text = 'Estimated Diameter (meters) (max/min): ' + str(round(estDiameter['estimated_diameter_max'],2)) + '/' + str(round(estDiameter['estimated_diameter_min'],2)))
+            
+    def search(self):
+        startDate = self.ids.start_date_label.text
+        endDate = self.ids.end_date_label.text
+        response = requests.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=" + startDate + "&end_date=" + endDate + "&api_key=eLLQHjpU93iCInMRF8Cvrm3x2wLoQrtDGsinjF48")
+        responseDump = json.dumps(response.json(),sort_keys=True, indent=4)
+        loadedDump = json.loads(responseDump)
+        idList = []
+        objectNames = []
+        allObjects = loadedDump['near_earth_objects']
+        keyList = list(allObjects.keys())
+        for i in range(len(allObjects)):
+            for j in range(len(allObjects[keyList[i]])):
+                idList.append(int(allObjects[keyList[i]][j]['id']))
+                objectNames.append(allObjects[keyList[i]][j]['name'])
+        for i in range(len(objectNames)):
+            self.ids.list_of_objects.add_widget(TwoLineListItem(text='Name: ' + objectNames[i], 
+                                                                secondary_text='Object SPK ID: ' + str(idList[i]),
+                                                                on_release = lambda x, objId = idList[i]: self.selected_object(objId)))
+        
+    
+    def show_date_picker(self,button_pressed):
+        date_dialog = MDDatePicker()
+        if button_pressed.name == 1:
+            date_dialog.bind(on_save=self.select_start_date)
+            date_dialog.open()
+        if button_pressed.name == 2:
+            date_dialog.bind(on_save=self.select_end_date)
+            date_dialog.open()   
+            
+    
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = 'BlueGray'
+        self.theme_cls.accent_palette = 'Red'
+        return MyLayout()
+    
+if __name__ == '__main__':
+    MainApp().run()
